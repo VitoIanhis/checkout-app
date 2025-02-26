83997e8f4b31db58a8ae75d195e67b97b01df30b
VitoIanhis <vitosiscarodeveloper@gmail.com>
2025-02-25 22:10:04 -0300

    feat: add restaurant menu page with categories and products

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantsHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -29,6 +30,7 @@ const RestaurantMenuPage = async ({
   return (
     <div>
       <RestaurantsHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
     </div>
   );
 };
