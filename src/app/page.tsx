c86328fd0f7870a8f6d20511e266ff886a9f028a
VitoIanhis <vitosiscarodeveloper@gmail.com>
2025-02-27 19:48:53 -0300

    removing dead code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 import { ChefHat } from "lucide-react";
 import Link from "next/link";
-import { notFound } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
-import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
 
 const InitialPage = async () => {
-  const restaurants = await getRestaurantBySlug("fsw-donalds");
-  if (!restaurants) {
-    return notFound();
-  }
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-4 bg-red-600">
       <ChefHat color="white" width={150} height={150} />
